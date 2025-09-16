55427b64d67bbb32f7deed2b646b891a7e8acc06
Kir <kozinkirill85@gmail.com>
2025-09-16 23:12:39 +0300

    fix: убраны дублирующиеся импорты в test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,7 @@
 """Тесты для функций модуля widget."""
 import pytest
-from src.widget import mask_account_card, get_date
+
+from src.widget import get_date, mask_account_card
 
 
 # Тесты для mask_account_card
